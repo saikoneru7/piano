1aef99a4d24cade53ba2cc20d02616860ffbab2e
Julian Ramirez <32346479+rjuliang@users.noreply.github.com>
2022-02-16 12:46:34 -0600

    Correcting syntax error

--- parentStore.py
+++ parentStore.py
@@ -3,17 +3,17 @@ import board
 import neopixel
 
 def getInitData(num_lights, color_on, color_off):
-    mainInitData={
-            num_lights = num_lights
-            color_on = color_on
-            color_off = color_off
-            next_light = 0
-            prev_light = 0
-            pixels = neopixel.NeoPixel(board.D18,
-                                        self.num_lights,
-                                        brightness=1,
-                                        pixel_order=neopixel.RGB)
-    }
+    #mainInitData={
+    mainInitData.num_lights = num_lights
+    mainInitData.color_on = color_on
+    mainInitData.color_off = color_off
+    mainInitData.next_light = 0
+    mainInitData.prev_light = 0
+    mainInitData.pixels = neopixel.NeoPixel(board.D18,
+                                self.num_lights,
+                                brightness=1,
+                                pixel_order=neopixel.RGB)
+    #}
     mainInitData.pixels.fill((0,0,0))
     return mainInitData
 
